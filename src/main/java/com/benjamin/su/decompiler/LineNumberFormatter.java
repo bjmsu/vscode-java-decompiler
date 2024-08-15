367d521f96518968060180393332c2cec4cd607b
Benjamin <johndoe@example.com>
2024-08-16 01:25:00 +0800

    Plugin build successfully

--- src/main/java/com/benjamin/su/decompiler/LineNumberFormatter.java
+++ src/main/java/com/benjamin/su/decompiler/LineNumberFormatter.java
@@ -9,25 +9,17 @@ import java.io.StringWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.EnumSet;
 import java.util.Iterator;
 import java.util.List;
 import com.strobel.decompiler.languages.LineNumberPosition;
 
 class LineNumberFormatter {
     private final List<LineNumberPosition> _positions;
-    private final EnumSet<LineNumberOption> _options;
     private final String _context;
 
-    public enum LineNumberOption {
-        LEADING_COMMENTS,
-        STRETCHED,
-    }
-
-    public LineNumberFormatter(String context, List<LineNumberPosition> lineNumberPositions, EnumSet<LineNumberOption> options) {
+    public LineNumberFormatter(String context, List<LineNumberPosition> lineNumberPositions) {
         _context = context;
         _positions = lineNumberPositions;
-        _options = (options == null ? EnumSet.noneOf(LineNumberOption.class) : options);
     }
 
     public String  reformatFile() throws IOException{
@@ -50,13 +42,8 @@ class LineNumberFormatter {
                     brokenLines.add(r.readLine());
                     numLinesRead++;
                 }
-
-                // Read the line that contains the next line number annotations, but don't write
-                // it yet.
                 String line = r.readLine();
                 numLinesRead++;
-
-                // See if there are two original line annotations on the same emitted line.
                 LineNumberPosition nextPos;
                 int prevPartLen = 0;
                 char[] indent = {};
@@ -65,7 +52,6 @@ class LineNumberFormatter {
                     if (nextPos != null
                             && nextPos.getEmittedLine() == pos.getEmittedLine()
                             && nextPos.getOriginalLine() > pos.getOriginalLine()) {
-                        // Two different source line numbers on the same emitted line!
                         posIndex++;
                         lineOffset++;
                         String firstPart = line.substring(0, nextPos.getEmittedColumn() - prevPartLen - 1);
@@ -75,7 +61,6 @@ class LineNumberFormatter {
                         Arrays.fill(indent, ' ');
                         line = line.substring(firstPart.length(), line.length());
 
-                        // Alter the position while adding it.
                         o_LineBrokenPositions.add(new LineNumberPosition(
                                 nextPos.getOriginalLine(), nextPos.getEmittedLine() + lineOffset, nextPos
                                         .getEmittedColumn()));
@@ -84,11 +69,9 @@ class LineNumberFormatter {
                     }
                 } while (nextPos != null);
 
-                // Nothing special here-- just emit the line.
                 brokenLines.add(new String(indent) + line);
             }
 
-            // Copy out the remainder of the file.
             String line;
             while ((line = r.readLine()) != null) {
                 brokenLines.add(line);
@@ -105,21 +88,12 @@ class LineNumberFormatter {
         int maxLineNo = LineNumberPosition.computeMaxLineNumber(lineBrokenPositions);
         StringWriter s=new StringWriter();
         LineNumberPrintWriter w = new LineNumberPrintWriter(maxLineNo, new BufferedWriter(s));
-        if (!_options.contains(LineNumberOption.LEADING_COMMENTS)) {
-            w.suppressLineNumbers();
-        }
-
-        boolean doStretching = true;//(_options.contains(LineNumberOption.STRETCHED));
-
         for (LineNumberPosition pos : lineBrokenPositions) {
             int nextTarget = pos.getOriginalLine();
             int nextActual = pos.getEmittedLine();
             int requiredAdjustment = (nextTarget - nextActual - globalOffset);
 
-            if (doStretching && requiredAdjustment < 0) {
-                // We currently need to remove newlines to squeeze things together.
-                // prefer to remove empty lines,
-                // 1. read all lines before nextActual and remove empty lines as needed
+            if (requiredAdjustment < 0) {
                 List<String> stripped = new ArrayList<>();
                 while (numLinesRead < nextActual - 1) {
                     String line = lines.next();
@@ -131,7 +105,6 @@ class LineNumberFormatter {
                         stripped.add(line);
                     }
                 }
-                // 2. print non empty lines while stripping further as needed
                 int lineNoToPrint = (stripped.size() + requiredAdjustment <= 0)
                         ? nextTarget
                         : LineNumberPrintWriter.NO_LINE_NUMBER;
@@ -145,7 +118,6 @@ class LineNumberFormatter {
                         w.println(lineNoToPrint, line);
                     }
                 }
-                // 3. read and print next actual
                 String line = lines.next();
                 numLinesRead++;
                 if (requiredAdjustment < 0) {
@@ -162,9 +134,7 @@ class LineNumberFormatter {
                     numLinesRead++;
                     boolean isLast = (numLinesRead >= nextActual);
                     int lineNoToPrint = isLast ? nextTarget : LineNumberPrintWriter.NO_LINE_NUMBER;
-
-                    if (requiredAdjustment > 0 && doStretching) {
-                        // We currently need to inject newlines to space things out.
+                    if (requiredAdjustment > 0 ) {
                         do {
                             w.println("");
                             requiredAdjustment--;
@@ -172,14 +142,11 @@ class LineNumberFormatter {
                         } while (isLast && requiredAdjustment > 0);
                         w.println(lineNoToPrint, line);
                     } else {
-                        // No tweaks needed-- we are on the ball.
                         w.println(lineNoToPrint, line);
                     }
                 }
             }
         }
-
-        // Finish out the file.
         String line;
         while (lines.hasNext()) {
             line = lines.next();
@@ -192,10 +159,7 @@ class LineNumberFormatter {
 
     private class LineNumberPrintWriter extends PrintWriter {
         public static final int NO_LINE_NUMBER = -1;
-        private final String _emptyPrefix;
         private final String _format;
-        private boolean _needsPrefix;
-        private boolean _suppressLineNumbers;
 
         public LineNumberPrintWriter(int maxLineNo, Writer w) {
             super(w);
@@ -205,12 +169,6 @@ class LineNumberFormatter {
             String samplePrefix = String.format(_format, maxLineNo);
             char[] prefixChars = samplePrefix.toCharArray();
             Arrays.fill(prefixChars, ' ');
-            _emptyPrefix = new String(prefixChars);
-            _needsPrefix = true;
-        }
-
-        public void suppressLineNumbers() {
-            _suppressLineNumbers = true;
         }
 
         @Override
@@ -224,26 +182,12 @@ class LineNumberFormatter {
         }
 
         public void println(int lineNumber, String s) {
-            this.doPrefix(lineNumber);
             super.println(s);
-            _needsPrefix = true;
         }
 
         public void print(int lineNumber, String s) {
-            this.doPrefix(lineNumber);
             super.print(s);
         }
 
-        private void doPrefix(int lineNumber) {
-            if (_needsPrefix && !_suppressLineNumbers) {
-                if (lineNumber == NO_LINE_NUMBER) {
-                    super.print(_emptyPrefix);
-                } else {
-                    String prefix = String.format(_format, lineNumber);
-                    super.print(prefix);
-                }
-            }
-            _needsPrefix = false;
-        }
     }
 }
--- vsix_build/README.md
+++ vsix_build/README.md
@@ -1,21 +1,28 @@
-# 2021.07.18 Update
+# 2024.08.15 Update
+1. Remove fernflower/cfr
+2. Using org.jboss.windup.decompiler.procyon.LineNumberPrintWriter
+     and org.jboss.windup.decompiler.procyon.LineNumberFormatter
+   to format source code for better debug.
+3. Build up the project, will support decomoile class file directly, and jar file.
 
+
+# 2021.07.18 Update
 1. Update procyon to support JDK11.
 2. When showDebugLineNumbers of procyon is true, align debug line to original line.
 
+
 # Decompiler for Java&trade; in Visual Studio Code
+*  This extension allows you to decompile Java class files. It requires [Language Support for Java&trade; by Red Hat](https://marketplace.visualstudio.com/items?itemName=redhat.java), version 0.12.0 or greater.
 
-This extension allows you to decompile Java class files. It requires [Language Support for Java&trade; by Red Hat](https://marketplace.visualstudio.com/items?itemName=redhat.java), version 0.12.0 or greater.
+*  To see the decompiler in action, right-click on a Java symbol for which you don't have the source code, and choose Go to Definition (or simply command/ctrl+click on the symbol). You will see the decompiled code.
 
-To see the decompiler in action, right-click on a Java symbol for which you don't have the source code, and choose Go to Definition (or simply command/ctrl+click on the symbol). You will see the decompiled code.
 
 ## Requirements
+*  [Language Support for Java&trade; by Red Hat](https://marketplace.visualstudio.com/items?itemName=redhat.java), version 0.12.0 or greater. This extension does not work with older versions.
 
-[Language Support for Java&trade; by Red Hat](https://marketplace.visualstudio.com/items?itemName=redhat.java), version 0.12.0 or greater. This extension does not work with older versions.
 
 ## Extension Settings
-
 You can use the following settings to customize the decompiler:
 
-* `java.contentProvider.preferred` (settings.json only): the ID of a decompiler to use. Currently, `fernflower`, `cfr` and `procyon` are supported. Defaults to `fernflower`.
-* `java.decompiler.[id]` (replace `[id]` with the ID of the decompiler you wish to configure): additional configuration to provide to the decompiler. The format depends on the chosen decompiler. Use the autocomplete functionality of Visual Studio Code's settings to view the possible options and their descriptions.
+* `java.contentProvider.preferred` (settings.json only): the ID of a decompiler to use. Currently, only `procyon` are supported..
+* `java.decompiler.procyon` : additional configuration to provide to the decompiler. The format depends on the chosen decompiler. Use the autocomplete functionality of Visual Studio Code's settings to view the possible options and their descriptions.
